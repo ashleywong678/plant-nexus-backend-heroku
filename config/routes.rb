d52cde6be828f9d35f4671ff0a78d81716794142
Ashley <ashleywong678@gmail.com>
2019-08-29 21:30:35 -0400

    added api route for plants

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :plants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #plant-nexus.com/api/v1/resource
   namespace :api do
     namespace :v1 do 
       resources :users
+      resources :plants
     end
   end
 
