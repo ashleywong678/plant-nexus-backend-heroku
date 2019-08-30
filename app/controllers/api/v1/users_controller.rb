a15ee60280e30911f0604cbe70a1d681cc0b0550
Ashley <ashleywong678@gmail.com>
2019-08-29 21:16:58 -0400

    moved user_controller into api/v1 folder

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
-class UsersController < ApplicationController
-  before_action :set_user, onlyi: [:show, :update, :destroy]
+class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
-  # def index
-  #   @users = User.all
-  #   render json: @users
-  # end
+  def index
+    @users = User.all
+    render json: @users
+  end
 
   # GET /users/1
-  # def show
-  #   render json: @user
-  # end
+  def show
+    render json: @user
+  end
 
   #POST /users
   def create
