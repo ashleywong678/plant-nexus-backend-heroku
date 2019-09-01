78446044ace9ae761b15931fc20bac967178ddfe
Ashley <ashleywong678@gmail.com>
2019-09-01 08:05:13 -0400

    added get route for current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   #plant-nexus.com/api/v1/resource
   namespace :api do
     namespace :v1 do 
