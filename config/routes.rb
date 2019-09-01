ba7e7a91d621eb94d20bc5f79bc58be2a813c2eb
Ashley <ashleywong678@gmail.com>
2019-09-01 11:13:47 -0400

    added delete route to log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  post "/api/v1/login", to: "api/v1/sessions#create"
-  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  post "/api/v1/login", to: "api/v1/sessions#create" #to log user in
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user" #to send data back about who's logged in
+  delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
+
   #plant-nexus.com/api/v1/resource
   namespace :api do
     namespace :v1 do 
