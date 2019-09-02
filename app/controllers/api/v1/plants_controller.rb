dc72f07ba2abf2e82b18a9e1e9fcb8c35ca022ed
Ashley <ashleywong678@gmail.com>
2019-09-02 13:00:29 -0400

    changed action notes to be nested updated index to get plants by user_id

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,18 +1,18 @@
 class Api::V1::PlantsController < ApplicationController
   before_action :set_plant, only: [:show, :update, :destroy]
 
-  # GET /plants
+  # GET users/:user_id/plants
   def index
-    @plants = Plant.all
+    @plants = User.find_by(id: params[:user_id]).plants
     render json: @plants
   end
 
-  # GET /plants/1
+  # GET users/:user_id/plants/1
   def show
     render json: @plant
   end
 
-  #POST /users
+  #POST /plants
   def create
     @user = User.new(user_params)
     if @user.save
