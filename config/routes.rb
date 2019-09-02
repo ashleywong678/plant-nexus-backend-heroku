13f4be6d5f12d0ff5178840bf4e62783f5cd7a0b
Ashley <ashleywong678@gmail.com>
2019-09-02 12:59:33 -0400

    edit synatx error, forgot , on nested route

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :users do
-        resources :plants only: [:index, :show]
+        resources :plants, only: [:index, :show]
       end
     end
   end
