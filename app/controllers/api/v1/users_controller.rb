341e21857ee1359f5f7f4b665f30c1fd072fd161
Ashley <ashleywong678@gmail.com>
2019-09-04 15:30:19 -0400

    logged user in after signs up, set up errors to render as json if user not created successfully, updated strong params from password_digest to password

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,9 +17,13 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
@@ -44,7 +48,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :username, :password_digest)
+    params.require(:user).permit(:name, :username, :password)
   end
 
 end
