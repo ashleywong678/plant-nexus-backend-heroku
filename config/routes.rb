73be6b0fcd92d69f0a17318c7fbe7e2b72b7c2ba
Ashley <ashleywong678@gmail.com>
2019-09-04 15:32:05 -0400

    added signup route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create" #to log user in
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user" #to send data back about who's logged in
   delete '/api/v1/logout', to: 'api/v1/sessions#destroy'
+  post "/api/v1/signup", to: "api/v1/users#create" #to log user in
+
 
   #plant-nexus.com/api/v1/resource
   namespace :api do
