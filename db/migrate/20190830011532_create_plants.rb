80493779989767674b2e42a78686998f626a9a8c
Ashley <ashleywong678@gmail.com>
2019-09-11 14:25:12 -0400

    rmved default img link b/c it was being overwritten with react, added a notes section

--- db/migrate/20190830011532_create_plants.rb
+++ db/migrate/20190830011532_create_plants.rb
@@ -4,6 +4,7 @@ class CreatePlants < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :scientific_name
       t.string :image_url
+      t.text :notes
       t.integer :user_id
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2019_08_30_011532) do
     t.string "name"
     t.string "scientific_name"
     t.string "image_url"
+    t.text "notes"
     t.integer "user_id"
   end
 
